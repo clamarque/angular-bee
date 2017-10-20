7fa4ffbb8339c5b0052fe22d94bc15379e85f112
anemonep <anemone.prangere@gmail.com>
2017-10-20 16:31:14 +0200

    History

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-history',
@@ -6,8 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.sass']
 })
 export class HistoryComponent implements OnInit {
+  userId : string;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { 
+    this.userId = firebase.auth().currentUser.uid;   
+    console.log("UserId : " + this.userId); 
+
+    firebase.database().ref('/items/' + this.userId).once('value').then(function(snapshot) {
+      console.log(snapshot);
+      // ...
+    });
+  }
 
   ngOnInit() {
   }
